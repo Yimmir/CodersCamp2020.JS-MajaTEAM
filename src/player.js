0ac26f6e1f41461b9f6cc6cf16fef875f13c8fe3
Adam Spinek <adamspinek@gmail.com>
2020-12-29 13:48:03 +0100

    feat(player.js): add player object

---
+++ src/player.js
@@ -0,0 +1,16 @@
+const player = {
+    name: '',
+    score: 0,
+    get name() {
+        return this.name;
+    },
+    set name (name) {
+        this.name = name;
+    },
+    get score() {
+        return this.score;
+    },
+    set score(score) {
+        this.score = score;
+    }
+}
