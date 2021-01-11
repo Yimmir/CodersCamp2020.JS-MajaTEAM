77091ec82a424aef5f8087a9b4a89eec187da163
marlexxa <marlexxa@gmail.com>
2021-01-11 17:22:16 +0100

    feat(questionGenartor.js): add function to generate wrong answers

--- src/app/questionGenerator.js
+++ src/app/questionGenerator.js
@@ -16,3 +16,21 @@ const getCorrectAnswer = async(id) => {
   const data = await API.byID(id);
   return {content: data.name, isCorrect: true}
 }
+
+const getIncorrectAnswers = async(correctID) => {
+  const availableAnswersIDs = [...getAvailableIDs(mode)];
+  const answersIDs= [];
+  let index;
+  const correctIndex = availableAnswersIDs.indexOf(correctID);
+  availableAnswersIDs.splice(correctIndex, 1);
+  for (let i=0; i<3; i++) {
+    index = getRandomIntInclusive(0, availableAnswersIDs.length-1)
+    answersIDs.push(availableAnswersIDs[index]);
+    availableAnswersIDs.splice(index, 1);
+  }
+  const incorrectAnswers = await Promise.all([...answersIDs].map(async id => {
+    const data = await API.byID(id);
+    return {content: data.name, isCorrect: false}
+  }));
+  return incorrectAnswers;
+}
