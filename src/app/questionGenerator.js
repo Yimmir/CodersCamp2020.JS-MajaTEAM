61ec84baa751fc52399a9b148379bd9a04cc2ab8
marlexxa <marlexxa@gmail.com>
2021-01-11 17:17:58 +0100

    feat(questionGenartor.js): add function to get question image

---
+++ src/app/questionGenerator.js
@@ -0,0 +1,13 @@
+import {loadDataFromAPI, loadImage} from './apiConnection'
+import {getAvailableIDs} from './gameModes'
+import {getRandomIntInclusive} from './random'
+
+const imageURL = 'static/assets/img/modes'
+const apiURL = 'https://swapi.dev/api'
+const mode = 'starships';                     //for testing
+const API = loadDataFromAPI(apiURL, mode);
+const image = loadImage(imageURL, mode);
+
+const getQuestionImage = async(id) => {
+  return await image.byID(id);
+}
