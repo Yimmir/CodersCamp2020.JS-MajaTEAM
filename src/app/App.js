4f914f514e2ac55c32ed7554abe8305e9632e250
DariuszKny <nasz_email_do_github>
2021-01-14 23:45:44 +0100

    feat(html): added highScores with styling

--- src/app/App.js
+++ src/app/App.js
@@ -1,4 +1,5 @@
 import { player } from "./player.js";
+import {getLocalScores} from  "./localScoreboard"
 
 export const App = ({options}) => {
 
@@ -6,10 +7,15 @@ const divStart = document.getElementById("div-start");
 const divPlay = document.getElementById("div-play");
 const divMenu = document.getElementById("div-menu");
 const divCategories = document.getElementById("div-categories");
+const divScores = document.getElementById("div-highScores");
+const divScoresLocalList = document.querySelector(".scoresListLocal");
+const divScoresGlobalList = document.querySelector(".scoresListGlobal");
 
 const btnPlay = document.getElementById("button-play");
 const btnNext = document.getElementById("button-next");
 const btnStart = document.getElementById("button-start");
+const btnHighScores = document.getElementById("button-highscores");
+const btnHighScoresBack = document.getElementById("button-back");
 
 btnPlay.addEventListener('click', () => {
     divPlay.style.display = "none";
@@ -30,7 +36,29 @@ btnNext.addEventListener('click', () => {
 btnStart.addEventListener('click', () => {
     divMenu.style.display = "none";
     divCategories.style.display = "block";
-})
+});
+
+btnHighScores.addEventListener('click', () => {
+    divMenu.style.display = "none";
+    divScores.style.display = "block";
+    let  scores = getLocalScores();
+    let lp =0;
+    scores.forEach((value) => {
+         console.log(value.name);
+         let li = document.createElement("li");
+         li.appendChild(document.createTextNode(`${lp}. ${value.name} ${value.score}`));
+        divScoresLocalList.appendChild(li);
+         lp++;
+     });
+    console.log(scores);
+    })
+
+    btnHighScoresBack.addEventListener('click', () => {
+        divMenu.style.display = "block";
+        divScores.style.display = "none";
+        divScoresLocalList.innerHTML = '';
+        divScoresGlobalList.innerHTML = '';
+    })
 
 }
 
