5cbaa543e66bee6878250ee9c9980b6798eed575
DariuszKny <nasz_email_do_github>
2021-01-16 16:39:46 +0100

    feat(scoreboard): moved functionality to scoreboards modules

--- src/app/globalScoreboard.js
+++ src/app/globalScoreboard.js
@@ -1,13 +1,43 @@
 let globalScores = [];
+let ulDivs = [document.querySelector(".rank-global"),
+  document.querySelector(".name-global"),
+  document.querySelector(".time-global")] ;
 const url = 'https://swquizzscoreboard123-default-rtdb.firebaseio.com/scores.json';
 
 
 /*The function should return already formatted data -> JSON object should be first
  mapped to array of scores*/
 
+
+
 const getGlobalScores = async function() {
-   await fetchData();
-   return globalScores
+   await fetchData().then(() => {
+     if(globalScores) {
+       globalScores.sort(compareScores);
+       let globalLp = 1;
+       globalScores.forEach((value) => {
+         ulDivs.forEach((value1) => {
+           let li = document.createElement("li");
+           let str = value1.className;
+           switch (str) {
+             case "result rank-global":
+               li.innerHTML = `${globalLp}`;
+               break;
+             case "result name-global":
+               li.innerHTML = `${value.name}`;
+               break;
+             case "result time-global":
+               li.innerHTML = `${value.score}`;
+               break;
+           }
+           value1.appendChild(li)
+         });
+         globalLp++
+       })
+     }
+
+     }
+   );
 };
 
 const saveScoreGlobally = async function(playerName, playerScore) {
@@ -39,10 +69,18 @@ const fetchData = async function() {
     });
 };
 
+const compareScores = function(a, b) {
+  return  a.score - b.score;
+};
+
+const clearGlobalScoreList = function() {
+  ulDivs.forEach(ul => ul.innerHTML ='')
+}
+
 
 /*This functionalities is connected to retrieve global scores from database -> The DB is
  created on Google Firebase so it is not standalone service , It is only a storage for
   json files that you can create as You want without strick restriction of usuall
    API.  */
 
-export {getGlobalScores, saveScoreGlobally};
+export {getGlobalScores, saveScoreGlobally, clearGlobalScoreList};
--- src/app/localScoreboard.js
+++ src/app/localScoreboard.js
@@ -1,12 +1,37 @@
 let localScores = [];
+let ulDivs = [document.querySelector(".rank-local"),
+  document.querySelector(".name-local"),
+  document.querySelector(".time-local")] ;
 
 const getLocalScores = function() {
   localScores = JSON.parse(localStorage.getItem("scores")) || [];
-  return localScores;
+  if(localScores) {
+    let localLp = 1;
+    localScores.forEach((value) => {
+      ulDivs.forEach((value1) => {
+        let li = document.createElement("li");
+        let str = value1.className;
+        switch (str) {
+          case "result rank-local":
+            li.innerHTML = `${localLp}`;
+            break;
+          case "result name-local":
+            li.innerHTML = `${value.name}`;
+            break;
+          case "result time-local":
+            li.innerHTML = `${value.score}`;
+            break;
+        }
+        value1.appendChild(li)
+      });
+      localLp++
+    })
+  }
 };
 
 const saveLocalScores = function(playerName, playerScore) {
-  let fetchedScores = getLocalScores();
+  localScores = JSON.parse(localStorage.getItem("scores")) || [];
+  let fetchedScores = localScores;
   fetchedScores.push({name: playerName, score: playerScore});
   localStorage.setItem("scores", JSON.stringify(sortLocalScores(fetchedScores)));
 };
@@ -16,12 +41,16 @@ const sortLocalScores = function(scores) {
 };
 
 const compareScores = function(a, b) {
-  return  b.score - a.score;
+  return  a.score - b.score;
+};
+
+const clearLocalScoreList = function() {
+  ulDivs.forEach(ul => ul.innerHTML ='')
 };
 
  /*getLocalSores is a function to retrieve the data from LocalStorage and
  saveLocalScores is function to save the last score to LocalStorage and compare to
  the others */
 
-export {getLocalScores, saveLocalScores};
+export {getLocalScores, saveLocalScores, clearLocalScoreList};
 
